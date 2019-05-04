6454c30f298d16059b0640cc9e6ce8a3e85d5c50
hixtrik <aricantaha06@gmail.com>
2019-05-04 12:58:59 +0300

    hixtrik
    part-14

---
+++ app/src/main/java/com/yunuscagliyan/socialreader/Profile/SignOutFragment.java
@@ -0,0 +1,25 @@
+package com.yunuscagliyan.socialreader.Profile;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yunuscagliyan.socialreader.R;
+
+public class SignOutFragment extends Fragment {
+
+    private static final String TAG = "SignOutFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_editprofile, container, false);
+
+
+        return view;
+    }
+
+}
