ed68bda6b617c90afb965f1aeb9b0c1e56030519
hixtrik <aricantaha06@gmail.com>
2019-05-11 11:43:58 +0300

    grup layoutu eklendi

--- app/src/main/java/com/yunuscagliyan/socialreader/Home/MessagesFragment.java
+++ app/src/main/java/com/yunuscagliyan/socialreader/Home/MessagesFragment.java
@@ -16,7 +16,7 @@ public class MessagesFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_messages, container, false);
+        View view = inflater.inflate(R.layout.fragment_groups, container, false);
 
         return view;
     }
